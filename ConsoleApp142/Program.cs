7c2dc495a8ec8533fac4c91e03809bd8142a640a
ygursu <yasemingrs@gmail.com>
2024-03-14 23:30:21 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp142/Program.cs
@@ -0,0 +1,31 @@
+﻿double sayi1 = 0, sayi2 = 0;
+string islem;
+Console.Write("Birinci sayıyı giriniz:");
+sayi1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("İkinci sayıyı giriniz:");
+sayi2 = Convert.ToDouble(Console.ReadLine());
+
+OkuOperator:
+Console.Write("Operatörlerinden(+,-,*,/) birini giriniz:");
+islem = Console.ReadLine();
+
+switch (islem)
+{
+    case "+":
+        Console.WriteLine("Toplam= {0}", (sayi1 + sayi2).ToString());
+        break;
+    case "-":
+        Console.WriteLine("Fark= {0}", (sayi1 - sayi2).ToString());
+        break;
+    case "*":
+        Console.WriteLine("Çarpım= {0}", (sayi1 * sayi2).ToString());
+        break;
+    case "/":
+        Console.WriteLine("Bölüm= {0}", (sayi1 / sayi2).ToString());
+        break;
+    default:
+        Console.WriteLine("Yanlış bir operatör girdiniz. Tekrar deneyin.");
+        goto OkuOperator;
+}
+
+Console.ReadKey();
